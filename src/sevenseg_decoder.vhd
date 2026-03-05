9aaa698289b359e997c927218d70779a74c3a315
henrywkrebs <henrywkrebs@gmail.com>
2026-03-04 21:48:51 -0700

    Working Version

--- src/sevenseg_decoder.vhd
+++ src/sevenseg_decoder.vhd
@@ -37,28 +37,41 @@ entity sevenseg_decoder is
 end sevenseg_decoder;
 
 architecture Behavioral of sevenseg_decoder is
-
-
+  signal seg_ah_abcdefg : std_logic_vector(6 downto 0); -- bit6=a ... bit0=g
+  signal seg_ah_AG      : std_logic_vector(6 downto 0); -- bit0=a ... bit6=g
 begin
 
-with i_Hex select
-o_seg_n <=  "1111110" when "0000",
-            "0110000" when "0001",
-            "1101101" when "0010",
-            "1111001" when "0011",
-            "0110011" when "0100",
-            "1011011" when "0101",
-            "1011111" when "0110",
-            "1110000" when "0111",
-            "1111111" when "1000",
-            "1110011" when "1001",
-            "1110111" when "1010",
-            "0011111" when "1011",
-            "0001101" when "1100",
-            "0111101" when "1101",
-            "1001111" when "1110",
-            "1000111" when "1111",
-            "0000000" when others;
-            
+  -- Active-HIGH truth table written as "abcdefg"
+  with i_Hex select
+  seg_ah_abcdefg <=
+    "1111110" when "0000",
+    "0110000" when "0001",
+    "1101101" when "0010",
+    "1111001" when "0011",
+    "0110011" when "0100",
+    "1011011" when "0101",
+    "1011111" when "0110",
+    "1110000" when "0111",
+    "1111111" when "1000",
+    "1110011" when "1001",
+    "1110111" when "1010",
+    "0011111" when "1011",
+    "0001101" when "1100",
+    "0111101" when "1101",
+    "1001111" when "1110",
+    "1000111" when "1111",
+    "0000000" when others;
+
+  -- Map abcdefg -> seg(0)=A ... seg(6)=G
+  seg_ah_AG(0) <= seg_ah_abcdefg(6); -- A
+  seg_ah_AG(1) <= seg_ah_abcdefg(5); -- B
+  seg_ah_AG(2) <= seg_ah_abcdefg(4); -- C
+  seg_ah_AG(3) <= seg_ah_abcdefg(3); -- D
+  seg_ah_AG(4) <= seg_ah_abcdefg(2); -- E
+  seg_ah_AG(5) <= seg_ah_abcdefg(1); -- F
+  seg_ah_AG(6) <= seg_ah_abcdefg(0); -- G
+
+  -- Convert to ACTIVE-LOW for Basys3 cathodes
+  o_seg_n <= not seg_ah_AG;
 
 end Behavioral;
